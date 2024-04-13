c6c151a792b060152f91158e3ccd1da9cf306bea
Jacob <kykiller80323@gmail.com>
2024-04-13 18:39:23 +0800

    upload new scene.

--- components/shooterComponents/Map.tsx
+++ components/shooterComponents/Map.tsx
@@ -4,7 +4,7 @@ import { RigidBody } from "@react-three/rapier";
 import React, { useEffect } from "react";
 import { Mesh } from "three/src/Three.js";
 
-const MAP_PATH = "./models/maps/scene_shoot_1.glb";
+const MAP_PATH = "./models/maps/scene_shoot_2.glb";
 export const Map = () => {
     const map = useGLTF(MAP_PATH);
 
